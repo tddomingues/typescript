a2b22d2d50d1cc35b5a1ba7668f56623bb2e43bd
tiago domingues <ttx8000@gmail.com>
2024-04-03 15:48:43 -0300

    instalacao e estudando os tipos básicos

---
+++ cursotypescript/src/A10001-type-annotation/index.ts
@@ -0,0 +1,23 @@
+const nome: string = 'Tiago';
+let idade: number = 21;
+let adulto: boolean = true;
+let simbolo: symbol = Symbol("qualquer-symbol");
+
+let arrayDeNumeros: Array<number> = [1, 2, 3];
+//ou
+let arrayDeNomes: string[] = ["Carlos", "Luana"];
+
+let objetoDePessoa: { nome: string, idade: number, adulto?: boolean } = {
+  nome: "Lucas",
+  idade: 21,
+  adulto: true //opcional
+}
+
+function soma(x: number, y: number): number {
+  return x + y
+}
+const soma2: (x: number, y: number) => number = (x, y) => {
+  return x / y
+}
+
+console.log(soma2(2, 3))
