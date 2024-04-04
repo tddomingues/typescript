733e06ae013850cb25ad8bd4c1f6d5ff939fad50
tiago domingues <ttx8000@gmail.com>
2024-04-04 13:07:02 -0300

    estudando o uso de undefined e null

---
+++ cursotypescript/src/A0006-null-undefined/index.ts
@@ -0,0 +1,26 @@
+//undefined = em variaveis opcionais('lastName?')
+function createPerson(firstName: string, lastName?: string): {
+  firstName: string;
+  lastName?: string;
+} {
+  return {
+    firstName, lastName
+  }
+}
+
+//null
+function squareOf(x: any) {
+  if (typeof x === "number") return x * x
+  return null
+}
+
+const squareOfTwoNumber = squareOf(3) //mude para "3" string para ver o erro
+
+//checar o null
+if (squareOfTwoNumber === null) {
+  console.log("Conta inválido por erro de tipagem")
+} else {
+  console.log(squareOfTwoNumber)
+}
+
+
